bd79014df74c1ed60c85e2cf25c96e79f6615150
Umair Tauni <engrumayr@gmail.com>
2024-04-30 11:50:08 +0500

    small changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ const messages = [
 ];
 
 export default function App() {
+  return (
+    <div>
+      <Steps />;
+      <Steps />;
+    </div>
+  );
+}
+
+function Steps() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
   // const [test, setTest] = useState({ name: "Jonas" });
@@ -31,7 +40,7 @@ export default function App() {
   }
 
   return (
-    <>
+    <div>
       <button className="close" onClick={() => setIsOpen((is) => !is)}>
         &times;
       </button>
@@ -65,6 +74,6 @@ export default function App() {
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 }
